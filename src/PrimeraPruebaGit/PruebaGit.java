a5b179bc02dc76905febcbff5e8c32a5fccf047d
Lucas-Turrer <63521780+Lucas-Turrer@users.noreply.github.com>
2021-04-10 09:53:00 -0300

    Primer commit

---
+++ src/PrimeraPruebaGit/PruebaGit.java
@@ -0,0 +1,11 @@
+package PrimeraPruebaGit;
+
+public class PruebaGit {
+
+	public static void main(String[] args) {
+		int i=20;
+		System.out.println("Primera Prueba en Git, el valor de i es: "+i);
+
+	}
+
+}
