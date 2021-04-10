334c98365973de7d64fd7888b89bd1c23f663fe6
Lucas-Turrer <63521780+Lucas-Turrer@users.noreply.github.com>
2021-04-10 10:11:34 -0300

    Incrementamos el valor de i

--- src/PrimeraPruebaGit/PruebaGit.java
+++ src/PrimeraPruebaGit/PruebaGit.java
@@ -5,6 +5,9 @@ public class PruebaGit {
 	public static void main(String[] args) {
 		int i=20;
 		System.out.println("Primera Prueba en Git, el valor de i es: "+i);
+		
+		i++;
+		System.out.println("ahora el valor de i es: "+i);
 
 	}
 
